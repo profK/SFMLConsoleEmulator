b3dcb9787415b3fd0dcf147ed73d694056ae3a4b
professorK <jeffpk@gmail.com>
2024-05-25 14:20:07 -0400

    removed old test project

--- SFMLConsoleEmulatorTest/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open SFMLConsoleEmulator
-
-[<EntryPoint>]
-let main argv =
-    Console.SetWindowSize(80, 80)
-    while true do ()
-    0 // return an integer exit code
